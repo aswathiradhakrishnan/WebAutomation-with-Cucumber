f4dee6d253d4726992add243c9feed9bbf1585f2
aswathiradhakrishnan <110847515+aswathiradhakrishnan@users.noreply.github.com>
2022-10-16 16:10:49 -0500

    Update AmazonPO.java

--- AmazonPO.java
+++ AmazonPO.java
@@ -8,13 +8,13 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
 import java.util.Properties;
-import java.util.concurrent.TimeUnit;
-
+import java.util.Set;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+
 
 public class AmazonPO {
 	public WebDriver driver;
@@ -22,13 +22,14 @@ public class AmazonPO {
 	By category = By.xpath("//a[text()=' Electronics ']");
 	By brand = By.xpath("//*[text()='OnePlus']");
 	By product = By.xpath("//*[contains(text(),'OnePlus Nord CE 2 Lite 5G (Blue Tide')]");
-	By allproducts = By.xpath("//*[@class='a-price-whole']");
-	By producttitle = By.xpath("//*[@id='productTitle'][contains(text(),'OnePlus Nord CE 2 Lite 5G')]");
-	By addtocart = By.xpath("//*[@id='add-to-cart-button']");
-	By cart =By.linkText("Cart");
+	By allprodDesc = By.xpath("//*[@class='a-price-whole']//preceding::*[@class='a-size-base-plus a-color-base a-text-normal']");
+	By allproductsprice = By.xpath("//*[@class='a-price-whole']");
+	By producttitle = By.xpath("//*[@id='productTitle']");
+	By addtocart = By.xpath("//input[@id='add-to-cart-button']");
+	By cart =By.xpath("//*[@id='attach-sidesheet-view-cart-button']/span/input");
 	By productinCart = By.xpath("//*[contains(text(),'OnePlus Nord CE 2 Lite 5G (Blue Tide')]");
-	
-	
+
+
 	//Constructor
 	public AmazonPO(WebDriver driver) {
 		this.driver = driver;
@@ -43,12 +44,16 @@ public class AmazonPO {
 
 	public WebElement getProduct() {
 		return driver.findElement(product);
-		
+
 	}
 
-	public List<WebElement> getProducts() {
-		return driver.findElements(allproducts);
+	/*public List<WebElement> getProducts() {
+		return driver.findElements(allprodDesc);
 	}
+	public List<WebElement> getPrices() {
+		return driver.findElements(allproductsprice); 
+	} */
+
 	public WebElement getproductTitle() {
 		return driver.findElement(producttitle);
 	}
@@ -59,12 +64,13 @@ public class AmazonPO {
 	public WebElement getCartbtn() {
 		return driver.findElement(cart);
 	}
+
 	public WebElement getProductinCart() {
 		return driver.findElement(productinCart);
 	}
-	
-	
-	
+
+
+
 	public void clickCategory() {
 		AmazonPO obj  = new AmazonPO(driver);
 		obj.getCategory().click();
@@ -78,7 +84,7 @@ public class AmazonPO {
 		AmazonPO obj = new AmazonPO(driver);
 		obj.getAddtoCart();
 	}
-	
+
 	public void getProductdesc() throws IOException {
 		AmazonPO obj = new AmazonPO(driver);
 		String getdesc = obj.getProduct().getText();
@@ -91,37 +97,65 @@ public class AmazonPO {
 		prop.setProperty("productdescription", getdesc);
 		prop.store(fr,"comments");
 	}
-	/*public void getProductsList() throws IOException {
-		AmazonPO obj = new AmazonPO(driver);
-		java.util.List<WebElement> rows = obj.getProducts();
-		int rows_count = rows.size();
-		for (int i= 0; i<rows_count; i++) {
-			java.util.List<WebElement> cols = (List<WebElement>) rows.get(i);
+	public void getProductsList() throws IOException {
+		//AmazonPO obj = new AmazonPO(driver);
+		//obj.getProducts();
+		//obj.getPrices();
+		List<WebElement> allproddesc = driver.findElements(allprodDesc);
+		List<WebElement> allprodprice = driver.findElements(allproductsprice);
+		int rowprodsize  = allproddesc.size();
+		int rowpricesize = allprodprice.size();
+
+		File file = new File("C:\\Users\\gsjij\\Eclipse_Appzlogic\\CucumberBDD\\ExcelFile\\Test.xls");
+		HSSFWorkbook workbook = new HSSFWorkbook();
+		HSSFSheet sheet = workbook.getSheet("ProductDetails");
+		for (WebElement currentdesc : allproddesc) {
+			String desc = currentdesc.getText();
+			for (int i = 0; i<rowprodsize-1; i++)
+			{
+				sheet.getRow(rowprodsize).createCell(0).setCellValue(desc);
+
+			}
 		}
+		for (WebElement currentprice : allprodprice) {
+			String price = currentprice.getText();
+			for (int i = 0; i<rowpricesize-1; i++)
+			{
+				sheet.getRow(rowpricesize).createCell(1).setCellValue(price);
+
+			}
 
 		}
-				HSSFWorkbook workbook = new HSSFWorkbook();
-		HSSFSheet sheet = workbook.createSheet();
-		sheet.createRow(0);
-		sheet.getRow(0).createCell(0);
-		File file = new File("C:\\Users\\gsjij\\Eclipse_Appzlogic\\CucumberBDD\\ExcelFile\\Test.xls");
-		FileOutputStream fos = new FileOutputStream(file)
+
+		FileOutputStream fos = new FileOutputStream(file);
 		workbook.write(fos);
 		workbook.close();
-	}*/
+	}
 
 	public void selectProduct() {
 		AmazonPO obj = new AmazonPO(driver);
-		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		obj.getProduct().click();
 	}
 	public boolean verifyProductdesc() throws IOException {
 		AmazonPO obj = new AmazonPO(driver);
-		obj.getProductdesc();
-		String productdesc = obj.getproductTitle().getText();
-		String getdesc = obj.getProduct().getText();
-		boolean value = productdesc.contains(getdesc);
-		return value;
+		boolean status = false;
+		String parenthandle = driver.getWindowHandle();
+		Set <String> handles = driver.getWindowHandles();
+		for (String handle : handles) {
+			if (!handle.equals(parenthandle)) {
+				driver.switchTo().window(handle);
+				String  productdesc = obj.getproductTitle().getText();
+				System.out.println("The description of product is same as the select product's description "+productdesc);
+				obj.getProductdesc();
+				String getdesc = obj.getProduct().getText();
+				if (productdesc.contains(getdesc)){
+					System.out.println("The description of product is same as the select product's description");
+					status = true;
+				}
+			}
+
+		}
+		return status;
 	}
 
 	public void selectAddtoCart() {
@@ -130,16 +164,29 @@ public class AmazonPO {
 	}
 
 
-	public void selectCartbtn() {
+	public void selectCartbtn() throws InterruptedException {
 		AmazonPO obj = new AmazonPO(driver);
-		obj.getCartbtn().click();
+		Thread.sleep(3000);
+				//driver.switchTo().frame(2);
+				obj.getCartbtn().click();
+			
+		
 	}
+	/*public boolean prodinCart() {
+		AmazonPO obj = new AmazonPO(driver);
+		boolean product = obj.getProductinCart().isEnabled();
+		return product;
+	} */
 
 	public boolean verifyCart() {
 		AmazonPO obj = new AmazonPO(driver);
-		String productdesc = obj.getproductTitle().getText();
+		boolean status = false;
+		String getdesc = obj.getProduct().getText();
 		String productinCart = obj.getProductinCart().getText();
-		boolean visible = productdesc.contains(productinCart);
-		return visible;
+		if (productinCart.contains(getdesc)){
+			System.out.println("The description of product is same as the select product's description");
+			status = true;
+		}
+		return status;
 	}
 }
